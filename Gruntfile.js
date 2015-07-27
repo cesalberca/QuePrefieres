783058604a97b6defb92585bd215679a82c5101f
Cesar <cesalberca@hotmail.es>
2015-07-27 17:02:23 +0200

    Now livereload works on mobile phones connected to the ip of the computer

--- Gruntfile.js
+++ Gruntfile.js
@@ -68,7 +68,7 @@ module.exports = function (grunt) {
       options: {
         port: 9000,
         // Change this to '0.0.0.0' to access the server from outside.
-        hostname: 'localhost',
+        hostname: '0.0.0.0',
         livereload: 35729
       },
       livereload: {
@@ -384,7 +384,14 @@ module.exports = function (grunt) {
           cwd: '.tmp/images',
           dest: '<%= yeoman.dist %>/images',
           src: ['generated/*']
-        }]
+        }, {
+            expand: true,
+            flatten: true,
+            cwd: 'bower_components/font-awesome/fonts',
+            dest: '<%= yeoman.dist %>/fonts',
+            src: ['*.*'],
+            filter: 'isFile'
+          }]
       },
       styles: {
         expand: true,
